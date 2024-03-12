b7fd94e4bc35e9c8e0a35bf5ec9802ad95e784f8
Arvinbuyan Otgonjargal <23LP6614@G301-CC0207.local>
2024-03-12 12:17:46 +0800

    finish

--- front/src/common/axios/index.ts
+++ front/src/common/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://fooddelivery-x50y.onrender.com",
   headers: {
     "Content-Type": "application.json",
   },
